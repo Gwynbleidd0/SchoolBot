14fb4e2473e1cabfdd781be7ec6da4db092eca1a
Gwynbleidd0 <gwinbleidd12@gmail.com>
2018-09-04 23:26:56 +0300

    like

--- Telega.py
+++ Telega.py
@@ -4,20 +4,21 @@ import telebot
 from telebot import apihelper
 from telebot import types
 
-#apihelper.proxy = {'https':'https://77.252.133.48:25772'}
+apihelper.proxy = {'https':'https://109.104.144.46:80'}
 bot = telebot.TeleBot(config.token)
 markup_start = types.ReplyKeyboardMarkup()
 markup_start.row('Получить расписание')
 markup_start.row('Инфо, так сказать')
+@bot.message_handler(commands=["start"])
+def random_messages(message):
+    bot.send_message(message.chat.id,"Это первая версия бота для 11А",reply_markup=markup_start)
 @bot.message_handler(content_types=["text"])
 def get_raspisanie(message):
     if message.text=='Получить расписание':
-        bot.send_message(message.chat.id,botT.get_timetable())
+        bot.send_message(message.chat.id,botT.get_timetable(),reply_markup=markup_start)
     if message.text=='Инфо, так сказать':
-        bot.send_message(message.chat.id,'v1.2 Все права принадлежат тому, кому принадлежат. Б класс пидоры. Аминь')
-@bot.message_handler(commands=["start"])
-def random_messages(message):
-    bot.send_message(message.chat.id,"Это первая версия бота для 11А")
+        bot.send_message(message.chat.id,'v1.2\nВсе права принадлежат тому, кому принадлежат.\nБ класс пидоры. Аминь',reply_markup=markup_start)
+
 if __name__ == '__main__':
 		bot.polling(none_stop=True)
     
